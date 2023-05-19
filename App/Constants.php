3fcef330e16c08d9389fefd27ac99911283217f7
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-19 01:38:43 -0400

    Fixed directory bug

--- App/Constants.php
+++ App/Constants.php
@@ -1,4 +1,4 @@
 <?php
 
 define("__PUBLIC__", __ROOT__ . DIRECTORY_SEPARATOR . "Public");
-define("__ASSETS__", __PUBLIC__ . DIRECTORY_SEPARATOR . "Assets");
+define("__ASSETS__", __PUBLIC__ . DIRECTORY_SEPARATOR . "assets");
